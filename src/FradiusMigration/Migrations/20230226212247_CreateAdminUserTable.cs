c623f0790b332f4c1c1ef248f3f33d3a66015f24
Ramon Almeida <ramongoncalves76@gmail.com>
2023-02-27 12:16:46 -0300

    fix: Id type and Date default

--- src/FradiusMigration/Migrations/20230226212247_CreateAdminUserTable.cs
+++ src/FradiusMigration/Migrations/20230226212247_CreateAdminUserTable.cs
@@ -12,15 +12,15 @@ namespace FradiusMigration.Migrations
         {
             migrationBuilder.CreateTable("tb_admin_user", columns: table => new
             {
-                Id = table.Column<int>(name: "admu_id", type:"INTEGER"),
+                Id = table.Column<int>(name: "admu_id", type:"SERIAL"),
                 FirstName = table.Column<String>(name: "admu_first_name", type:"VARCHAR (64)"),
                 LastName = table.Column<String>(name: "admu_last_name", type:"VARCHAR (64)"),
                 Username = table.Column<String>(name: "admu_username", type:"VARCHAR (32)"),
                 Email = table.Column<String>(name: "admu_email", type:"VARCHAR (64)"),
                 Password = table.Column<String>(name: "admu_password", type:"VARCHAR (72)"),
                 ProfileImage = table.Column<String>(name: "admu_profile_image", type:"VARCHAR (64)", nullable:true),
-                CreateAt = table.Column<DateTime>(name: "admu_create_at", type:"TIMESTAMP"),
-                UpdateAt = table.Column<DateTime>(name: "admu_update_at", type:"TIMESTAMP"),
+                CreateAt = table.Column<DateTime>(name: "admu_create_at", type:"TIMESTAMP", defaultValueSql:"CURRENT_TIMESTAMP"),
+                UpdateAt = table.Column<DateTime>(name: "admu_update_at", type:"TIMESTAMP", defaultValueSql:"CURRENT_TIMESTAMP"),
                 IsActive = table.Column<bool>(name: "admu_is_active", type:"BOOLEAN", defaultValueSql:"true")
             }, constraints: table =>
             {
