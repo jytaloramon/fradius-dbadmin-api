16e0b1339108cbe33f271d4e3ccd7c113a9521c0
Ramon Almeida <ramongoncalves76@gmail.com>
2023-02-27 12:18:12 -0300

    fix: default SGBD

--- src/FradiusMigration/DbConnection.cs
+++ src/FradiusMigration/DbConnection.cs
@@ -4,8 +4,7 @@ namespace FradiusMigration;
 
 public class DbConnection : FradiusDbConnection
 {
-    public DbConnection() : base(SgbdConnectionFacade.MariaDb(
-        "any", "any", "any", "any", new Version(10, 9, 5)))
+    public DbConnection() : base(SgbdConnectionFacade.Psql("", "", "", ""))
     {
     }
 }
