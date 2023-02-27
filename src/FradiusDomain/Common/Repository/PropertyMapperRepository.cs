3b81e17addc92a0d7e927212ec0faa5d2e4e106a
Ramon Almeida <ramongoncalves76@gmail.com>
2023-02-27 12:19:30 -0300

    fix: id ValueGenerated

--- src/FradiusDomain/Common/Repository/PropertyMapperRepository.cs
+++ src/FradiusDomain/Common/Repository/PropertyMapperRepository.cs
@@ -12,7 +12,7 @@ public static class PropertyMapperRepository
             modelBuilder.Entity<AdminUser>(builder =>
             {
                 builder.ToTable("tb_admin_user");
-                builder.Property(a => a.Id).HasColumnName("admu_id");
+                builder.Property(a => a.Id).HasColumnName("admu_id").ValueGeneratedOnAdd();
                 builder.Property(a => a.FirstName).HasColumnName("admu_first_name");
                 builder.Property(a => a.LastName).HasColumnName("admu_last_name");
                 builder.Property(a => a.Username).HasColumnName("admu_username");
@@ -23,6 +23,7 @@ public static class PropertyMapperRepository
                 builder.Property(a => a.UpdateAt).HasColumnName("admu_update_at").ValueGeneratedOnAddOrUpdate();
                 builder.Property(a => a.IsActive).HasColumnName("admu_is_active");
             });
+            
             return null;
         };
     }
